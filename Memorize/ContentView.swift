93b5ee480cc880dc74c3f3e1f859439b099fd06d
GreenRamSilgen <kiranshrs@yahoo.com>
2024-11-08 19:12:00 -0800

    toggleable isFaceUp

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -22,20 +22,21 @@ struct ContentView: View {
 }
 
 struct CardView : View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp: Bool = false
     var body: some View {
         ZStack {
+            let base : RoundedRectangle = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                    RoundedRectangle(cornerRadius: 12)
-                        .foregroundColor(.white)
-                    RoundedRectangle(cornerRadius: 12)
-                        .strokeBorder(lineWidth: 2)
-                    Text("👻")
-                        .font(.largeTitle)
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text("👻")
+                    .font(.largeTitle)
             } else {
-                
-                    RoundedRectangle(cornerRadius: 12)
+                base
             }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
