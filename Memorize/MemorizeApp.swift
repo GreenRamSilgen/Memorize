05b0376cebd41fbcd2ff4d31217adf30af528b54
GreenRamSilgen <kiranshrs@yahoo.com>
2024-11-08 18:55:53 -0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Kiran Shrestha on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
