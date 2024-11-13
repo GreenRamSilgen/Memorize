f0ea4bbc8cf3304a228110af385cbdce022a57de
GreenRamSilgen <kiranshrs@yahoo.com>
2024-11-12 20:25:25 -0800

    Added game logic Used built in functions Created extension for Array

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -5,4 +5,30 @@
 //  Created by Kiran Shrestha on 11/11/24.
 //
 
-import Foundation
+import SwiftUI
+
+class EmojiMemoryGame : ObservableObject {
+    private static let emojis = ["👻", "🎃", "🤖", "👽", "💀", "☠️"]
+    private static func createMemoryGame() -> MemoryGame<String> {
+        return MemoryGame(numberOfParisOfCards: 4) { pairIndex  in
+            if emojis.indices.contains(pairIndex) {
+                return emojis[pairIndex]
+            }else {
+                return "‼️"
+            }
+        }
+    }
+    @Published private var model = createMemoryGame()
+    
+    var cards : Array<MemoryGame<String>.Card>{
+        model.cards
+    }
+    // MARK: - Intents
+    func shuffle() {
+        model.shuffle()
+    }
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card)
+    }
+}
--- MemorizeGame.swift
+++
@@ -1,8 +0,0 @@
-//
-//  MemorizeGame.swift
-//  Memorize
-//
-//  Created by Kiran Shrestha on 11/11/24.
-//
-
-import Foundation
